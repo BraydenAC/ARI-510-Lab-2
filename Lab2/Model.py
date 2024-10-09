80179e14c8f858096f9216278dea270d03d84f13
Brayden Cloutier <135390195+BraydenAC@users.noreply.github.com>
2024-10-09 13:12:35 -0400

    DBSCAN and report
    Added DBSCAN and associated graph
    Completed most of the written report
    Fixed extra empty graphs bug

--- Lab2/Model.py
+++ Lab2/Model.py
@@ -2,7 +2,8 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.cluster import dbscan
 from sklearn.metrics import silhouette_score
 scaler = StandardScaler()
 
@@ -27,15 +28,15 @@ X_test_pca = pca.transform(X_test)
 
 #display hist
 sns.histplot(y_train)
-plt.show()
-plt.clf()
+# plt.show()
+plt.close()
 
 #display line graph
 explained_variance = pca.explained_variance_ratio_
 components = np.arange(1, len(explained_variance) + 1)
 sns.lineplot(x=components, y=explained_variance)
-plt.show()
-plt.clf()
+# plt.show()
+plt.close()
 
 #Scatter plot for first 2 principal components
 plt.figure(figsize=(10, 7))
@@ -48,10 +49,43 @@ plt.title("Scatter plot of the first 2 principal components")
 plt.xlabel("Principal Component 1")
 plt.ylabel("Principal Component 2")
 plt.grid(True)
-plt.show()
-plt.clf()
+# plt.show()
+plt.close()
 
 #Kmeans Clustering
 KMeans_Model = KMeans(n_clusters=2, random_state=42)
 KMeans_Model.fit(X_train)
+print("Silhouette Score:")
 print(silhouette_score(X_train, KMeans_Model.labels_))
+
+#Scatter plot for cluster labels using top 2 principle components
+plt.figure(figsize=(10, 7))
+scatter = plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1], c=KMeans_Model.labels_, cmap='viridis', edgecolor='k', s=100)
+
+legend1 = plt.legend(*scatter.legend_elements(), title="Classes")
+plt.gca().add_artist(legend1)
+
+plt.title("Scatter plot for cluster labels")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+plt.grid(True)
+# plt.show()
+plt.close()
+
+#DBSCAN
+dbscan_model = DBSCAN(eps=5.03, min_samples=45, metric='euclidean')
+dbscan_model.fit(X_train)
+
+#Scatter plot for DBSCAN
+plt.figure(figsize=(10, 7))
+scatter = plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1], c=dbscan_model.labels_, cmap='viridis', edgecolor='k', s=100)
+
+legend1 = plt.legend(*scatter.legend_elements(), title="Classes")
+plt.gca().add_artist(legend1)
+
+plt.title("Scatter plot for DBSCAN")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+plt.grid(True)
+plt.show()
+plt.close()
